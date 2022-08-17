9aa4a65c5cc7ecef00642e796855477822a17774
Samson Marikwa <samsonmarikwa@outlook.com>
2022-08-17 17:21:24 -0400

    Call the Fraud microservice from the Customer microservice using RestTemplate

--- samsonservices/customer/src/main/java/com/samsonmarikwa/customer/service/CustomerService.java
+++ samsonservices/customer/src/main/java/com/samsonmarikwa/customer/service/CustomerService.java
@@ -1,12 +1,14 @@
 package com.samsonmarikwa.customer.service;
 
 import com.samsonmarikwa.customer.dto.CustomerRegistrationRequest;
+import com.samsonmarikwa.customer.dto.FraudCheckResponse;
 import com.samsonmarikwa.customer.entity.Customer;
 import com.samsonmarikwa.customer.repository.CustomerRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
-public record CustomerService(CustomerRepository customerRepository) {
+public record CustomerService(CustomerRepository customerRepository, RestTemplate restTemplate) {
    public void registerCustomer(CustomerRegistrationRequest request) {
       Customer customer = Customer.builder()
             .firstName(request.firstName())
@@ -16,10 +18,18 @@ public record CustomerService(CustomerRepository customerRepository) {
       
       // todo: check if email is valid
       // todo: check if email not taken
-      // todo: check if fraudster
       
       // save customer record
       customerRepository.save(customer);
+      
+      // call fraud microservice
+      String url = "http://localhost:8081/api/v1/fraud-check/{customerId}";
+      FraudCheckResponse fraudCheck = restTemplate.getForObject(url, FraudCheckResponse.class, customer.getId());
+      
+      if (fraudCheck.isFraudster()) {
+         throw new IllegalStateException("Fraudster detected");
+      }
+      
       // todo: send notification
       
    }
--- samsonservices/fraud/src/main/java/com/samsonmarikwa/fraud/controller/FraudController.java
+++ samsonservices/fraud/src/main/java/com/samsonmarikwa/fraud/controller/FraudController.java
@@ -3,11 +3,13 @@ package com.samsonmarikwa.fraud.controller;
 import com.samsonmarikwa.fraud.dto.FraudCheckResponse;
 import com.samsonmarikwa.fraud.service.FraudCheckService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/fraud-check")
 @AllArgsConstructor
+@Slf4j
 /*
 public record FraudController(FraudCheckService fraudCheckService) {
 Below we have used a class to replace the record as done in the other CustomerController. A record was
@@ -20,6 +22,8 @@ public class FraudController {
    @GetMapping("/{customerId}")
    public FraudCheckResponse isFraudster(@PathVariable Integer customerId) {
       boolean isFraudlentCustomer = fraudCheckService.isFraudlentCustomer(customerId);
+      log.info("Fraud check request for customer {}", customerId);
+      
       return new FraudCheckResponse(isFraudlentCustomer);
    }
 }
