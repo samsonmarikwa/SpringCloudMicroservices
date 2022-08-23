3b91aedc6a1a89db2a5e14491073e4429ac17d65
Samson Marikwa <samsonmarikwa@outlook.com>
2022-08-23 12:58:30 -0400

    Configure the Rabbit Listener which allows us to receive messages from the queue

--- samsonservices/amqp/src/main/java/com/samsonmarikwa/amqp/RabbitMQConfig.java
+++ samsonservices/amqp/src/main/java/com/samsonmarikwa/amqp/RabbitMQConfig.java
@@ -2,6 +2,7 @@ package com.samsonmarikwa.amqp;
 
 import lombok.AllArgsConstructor;
 import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
@@ -15,6 +16,7 @@ public class RabbitMQConfig {
    
    private final ConnectionFactory connectionFactory;
 
+   // Allows us to send messages to the queue
    @Bean
    public AmqpTemplate amqpTemplate() {
       // By default, we get the RabbitTemplate but because we need to customise it,
@@ -25,8 +27,16 @@ public class RabbitMQConfig {
       return rabbitTemplate;
    }
    
+   // Allows us to receive messages from the queue
    @Bean
-   public MessageConverter getJackson2JsonMessageConverter() {
+   public SimpleRabbitListenerContainerFactory simpleRabbitListenerContainerFactory() {
+      SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+      factory.setConnectionFactory(connectionFactory);
+      factory.setMessageConverter(getJackson2JsonMessageConverter());
+      return factory;
+   }
+   
+   private static MessageConverter getJackson2JsonMessageConverter() {
       return new Jackson2JsonMessageConverter();
    }
 }
