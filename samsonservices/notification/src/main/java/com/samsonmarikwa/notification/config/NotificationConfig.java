ae41a807d8c3fe24c96199656b43bfedde1a44dc
Samson Marikwa <samsonmarikwa@outlook.com>
2022-08-23 15:31:39 -0400

    NotificationConfig - create Topic Exchange, Queue and create a Binding

---
+++ samsonservices/notification/src/main/java/com/samsonmarikwa/notification/config/NotificationConfig.java
@@ -0,0 +1,41 @@
+package com.samsonmarikwa.notification.config;
+
+import lombok.Getter;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class NotificationConfig {
+   
+   @Value("${rabbitmq.exchange.internal}")
+   private String internalExchange;
+   
+   @Value("${rabbitmq.queue.notification}")
+   private String notificationQueue;
+   
+   @Value("${rabbitmq.routing-key.internal-notification}")
+   private String internalNotificationRoutingKey;
+   
+   @Bean
+   public TopicExchange topicExchange() {
+      return new TopicExchange(this.internalExchange);
+   }
+   
+   @Bean
+   public Queue notificationQueue() {
+      return new Queue(this.notificationQueue);
+   }
+   
+   @Bean
+   public Binding topicExchangeToNotificationBinding() {
+      return BindingBuilder
+            .bind(notificationQueue())
+            .to(topicExchange())
+            .with(this.internalNotificationRoutingKey);
+   }
+}
