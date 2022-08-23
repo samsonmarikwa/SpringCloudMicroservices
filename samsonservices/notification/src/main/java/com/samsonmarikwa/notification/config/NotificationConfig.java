c245a00000a3ff39a4d7cefbcf11d417829e82c1
Samson Marikwa <samsonmarikwa@outlook.com>
2022-08-23 16:20:23 -0400

    Publish messages

--- samsonservices/notification/src/main/java/com/samsonmarikwa/notification/config/NotificationConfig.java
+++ samsonservices/notification/src/main/java/com/samsonmarikwa/notification/config/NotificationConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@Getter
 public class NotificationConfig {
    
    @Value("${rabbitmq.exchange.internal}")
